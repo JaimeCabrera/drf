9e0ea59ef55e368d2b931c4230f1385215f07556
Jaime Cabrera <jycabrerap@gmail.com>
2023-11-18 10:14:42 -0500

    add restfamework

--- api/users/api.py
+++ api/users/api.py
@@ -1,3 +1,5 @@
+from rest_framework import status
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -10,3 +12,43 @@ class UserApiView(APIView):
         users = User.objects.all()
         users_serializer = UserSerializer(users, many=True)
         return Response(users_serializer.data)
+
+
+# funciones
+@api_view(['GET', 'POST'])
+def user_api_view(request):
+    if request.method == "GET":
+        users = User.objects.all()
+        users_serializer = UserSerializer(users, many=True)
+        return Response(users_serializer.data, status=status.HTTP_200_OK)
+    elif request.method == "POST":
+        user_serializer = UserSerializer(data=request.data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response(user_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(user_serializer.errors)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def user_detail_view(request, pk=None):
+    # queryset
+    user = User.objects.filter(id=pk).first()
+    # validation
+    if user:
+        # retrieve
+        if request.method == 'GET':
+            user_serializer = UserSerializer(user)
+            return Response(user_serializer.data, status=status.HTTP_200_OK)
+        # update
+        elif request.method == 'PUT':
+            user_serializer = UserSerializer(user, data=request.data)
+            if user_serializer.is_valid():
+                user_serializer.save()
+                return Response(user_serializer.data, status=status.HTTP_200_OK)
+            return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        # delete
+        elif request.method == 'DELETE':
+            user.delete()
+            return Response({"msg": "Eliminado"}, status=status.HTTP_200_OK)
+
+    return Response({"msg": "No se ha encontrado el usuario"}, status=status.HTTP_400_BAD_REQUEST)
--- api/users/urls.py
+++ api/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from api.users.api import UserApiView
+from api.users.api import UserApiView, user_api_view, user_detail_view
 
 urlpatterns = [
-    path('user/', UserApiView.as_view(), name="user_api")
+    path('user/', UserApiView.as_view(), name="user_api"),
+    path('userd/', user_api_view, name="user_api_decorator"),
+    path('userd/<int:pk>', user_detail_view, name="user_detail_api")
 ]
